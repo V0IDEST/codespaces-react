f19c7d4068c8e0fae9608189bfa5e62c6e3f28b9
V0IDEST <ianlucasborges5@gmail.com>
2025-06-06 16:44:30 +0000

    commit pronto

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,46 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText.jsx";
 
 export default function App() {
+  const texts = [
+    {
+      title: "React Component 1",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora similique neque, ipsum at non expedita! Quisquam, voluptatibus. Quod, cumque! Quasi, doloribus.",
+    },
+    {
+      title: "React Component 2",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora similique neque, ipsum at non expedita! Quisquam, voluptatibus. Quod, cumque! Quasi, doloribus.",
+    },
+    {
+      title: "React Component 3",
+      text: "aaaaaai",
+    },
+    {
+      title: "React Component 4",
+      text: "aaaaaai",
+    },
+    {
+      title: "React Component 5",
+      text: "aaaaaai",
+    },
+  ];
+  const textComponents = texts.map((text, index) => (
+    <MyText key={index} title={`$ text.titl} ${index + 1}` text={text.text} />
+  ));
+  // const textComponents = texts.map((text, index) => (
+  //   <MyText key={index} title={text.title} text={text.text} />
+
   return (
     <>
-    <MyText />
-    <MyText />
-    <MyText />
+      {textComponents}
+
+      <MyText
+        title="React Component 1"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora similique neque, ipsum at non expedita! Quisquam, voluptatibus. Quod, cumque! Quasi, doloribus."
+      />
+      <MyText title="React Component 2" text="Lorem" />
+      <MyText title="React Component 3" text="aaaaaai" />
+      <MyText title="React Component 4" text="aaaaaai" />
+      <MyText title="React Component 5" text="aaaaaai" />
     </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,15 +1,16 @@
-import styles from "./MyText.module.css";
+import React from "react";
+import styles from "./MyText.Module.css";
 
-export function MyText({ text }) {
+
+// import styles from "./MyText.module.scss";
+
+export function MyText({ title, children }) {
   return (
     <div className={styles.container}>
       <div className={styles.div}>
-        <h1 className={styles.title}>Meu primeiro REACT app</h1>
+        <h1 className={styles.title}>{props.title}</h1>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
-          consequuntur nam explicabo expedita sequi fugit officia dolor rerum
-          enim aspernatur sapiente doloribus numquam dolorum laudantium, saepe
-          eos? Laboriosam, nobis magnam?
+          {props.children}
         </p>
       </div>
     </div>
