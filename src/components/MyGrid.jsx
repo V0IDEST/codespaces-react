4454dd4b5746030a3f1a40f8e4f0364f159d582f
V0IDEST <ianlucasborges5@gmail.com>
2025-06-13 18:07:56 +0000

    Concluído-mudarpastas

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -1,34 +1,126 @@
 import styles from "./MyGrid.module.css";
-
+import { Github, Instagram, Phone, BookOpenCheck } from "lucide-react";
+const Cards = [0,1,2,3,4];
 export function MyGrid() {
+  const randoNumbers = Cards.map(() => Math.floor(Math.random() * 300));
   return (
-    <div className={styles.container}>
-      <header className={styles.header} />
+      <div className={styles.container}>
+      <header className={styles.header}>
+        <BookOpenCheck />
+        <h1>Foco, Força, Fé</h1>
+      </header>
       <main className={styles.main}>
         <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>Card 1</h2>
-            <p>This is the first card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 2</h2>
-            <p>This is the second card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 3</h2>
-            <p>This is the third card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 4</h2>
-            <p>This is the fourth card.</p>
-          </div>
-          <div className={styles.card}>
-            <h2>Card 5</h2>
-            <p>This is the fifth card.</p>
-          </div>
+          {
+            <div className={styles.card}>
+              <img
+                src={"https://picsum.photos/200/300?random=${randomNumbers[0]}"}
+              ></img>
+              <h2>My Text 0</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          }
+          {
+            <div className={styles.card}>
+              <img
+                src={"https://picsum.photos/200/300?random=${randomNumbers[1]}"}
+              ></img>
+              <h2>My Text 1</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          }
+          {
+            <div className={styles.card}>
+              <img
+                src={"https://picsum.photos/200/300?random=${randomNumbers[2]}"}
+              ></img>
+              <h2>My Text 2</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          }
+          {
+            <div className={styles.card}>
+              <img
+                src={"https://picsum.photos/200/300?random=${randomNumbers[3]}"}
+              ></img>
+              <h2>My Text 3</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          }
+          {
+            <div className={styles.card}>
+              <img
+                src={"https://picsum.photos/200/300?random=${randomNumbers[4]}"}
+              ></img>
+              <h2>My Text 4</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          }
         </div>
       </main>
-      <footer className={styles.footer} />
+      <footer className={styles.footer}>
+        <div className={styles.footerContent}>
+          <div className={styles.footerText}>
+            <h1>IFRN - Campus Macau</h1>
+            <h1>Curso Técnico em Informática</h1>
+            <h1>Programação para Internet 2025</h1>
+          </div>
+          <div>
+            <p>Ian Lucas V Borges</p>
+          </div>
+          <div className={styles.icons}>
+            <a href="https://github.com/V0IDEST" target="_blank">
+              <Github />
+            </a>
+            <a href="https://instagram.com/i.anborges/" target="_blank">
+              <Instagram />
+            </a>
+            <a href="tel:+5584998189159" target="_blank">
+              <Phone />
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
